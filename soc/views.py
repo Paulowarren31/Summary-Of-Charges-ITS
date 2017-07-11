fbbea1d8c4d5da3fc39fc1a02ebfe2eb26315c0a
paulowar <paulowar@umich.edu>
2017-07-11 14:32:05 -0400

    forgot login

--- soc/views.py
+++ soc/views.py
@@ -15,7 +15,7 @@ import requests
 import base64
 
 # index view
-#@login_required(login_url='/accounts/login')
+@login_required(login_url='/accounts/login')
 #uniqname must be in the pinnacle authorized users table
 #@user_has_permission
 def index(request):
@@ -27,7 +27,7 @@ def index(request):
   return render(request, 'index.html', res)
 
 # table view
-#@login_required(login_url='/accounts/login')
+@login_required(login_url='/accounts/login')
 #@user_has_permission
 def table(request):
 
