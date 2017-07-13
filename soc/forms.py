4556a3e4fcf0bf903156f30caff4913c6c3333e2
paulowar <paulowar@umich.edu>
2017-07-13 11:42:25 -0400

    test

--- soc/forms.py
+++ soc/forms.py
@@ -3,7 +3,9 @@ from models import um_ecomm_dept_units_rept, Search
 from django.utils.dates import MONTHS
 
 class MainForm(forms.Form):
+
   dept_id = forms.CharField(label='Department ID', max_length=20, required=False)
+
   dept_id_range = forms.CharField(label='Department IDs', widget=forms.Textarea(attrs={'placeholder': 'Separate individual departments with commas, and ranges with dashes (481054, 481060-481065)'}), required=False)
 
   DEPT_GRPS = list(um_ecomm_dept_units_rept.objects.order_by().values_list('dept_grp', flat=True).distinct())
@@ -32,6 +34,10 @@ class MainForm(forms.Form):
   d_choice = forms.CharField(max_length=1)
   t_choice = forms.CharField(max_length=1)
 
+  def clean(self):
+    # logic for checking validness
+
   def save(self, dept, time):
     search = Search(dept=dept, time=time)
     search.save()
+
