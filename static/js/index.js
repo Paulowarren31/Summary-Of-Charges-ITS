c7844cf3a467923850584f6d0dea18ed091a4201
paulowar <paulowar@umich.edu>
2017-07-19 09:47:18 -0400

    index changes not updating hidden field

--- static/js/index.js
+++ static/js/index.js
@@ -42,21 +42,8 @@ $(function(){
 
   $('#submit-dept-btn').on('click', e => {
     let dept_ids = $('#id_dept_id_range').val()
-    deptUpdate(dept_ids, () => {
-      let prev = $('#dept_id_range_actual').val()
 
-      if(prev.length > 0){
-        $('#dept_id_range_actual').val( prev + ',' + dept_ids)
-      }
-      else{
-
-        $('#dept_id_range_actual').val(dept_ids)
-      }
-
-
-
-
-    })
+    deptUpdate(dept_ids)
   })
 
   deptUpdate($('#dept_id_range_actual').val())
@@ -84,7 +71,7 @@ function csrfSafeMethod(method) {
   return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
 }
 
-function deptUpdate(dept_ids, callback){
+function deptUpdate(dept_ids){
   let csrftoken = getCookie('csrftoken')
 
   //required for django
@@ -117,8 +104,18 @@ function deptUpdate(dept_ids, callback){
     })
 
     $('#id_dept_id_range').val('') //clear the box
+    console.log(depts)
 
-    if(depts.length > 0) callback()
+    if(depts.length > 0){
+      let prev = $('#dept_id_range_actual').val()
+
+      if(prev.length > 0){
+        $('#dept_id_range_actual').val( prev + ',' + dept_ids)
+      }
+      else{
+        $('#dept_id_range_actual').val(dept_ids)
+      }
+    } 
 
   })
 }
