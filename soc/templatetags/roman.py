780c358d8e1c458739e3388d50f79afb2ab9657f
Paulo Warren <paulowar@umich.edu>
2017-07-25 12:59:12 -0400

    typo

--- soc/templatetags/roman.py
+++ soc/templatetags/roman.py
@@ -5,9 +5,6 @@ register = template.Library()
 num_map = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'),
            (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')]
 
-def num2roman(num):
-
-    # should be a number
 @register.filter
 def roman(value):
   num = int(value)
