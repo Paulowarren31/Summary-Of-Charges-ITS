a3df7ec972034cf038a67572df2e012fae1d4ac8
Paulo Warren <paulowar@umich.edu>
2017-08-01 13:41:51 -0400

    trying something else for tree

--- soc/views.py
+++ soc/views.py
@@ -307,29 +307,57 @@ def search(request):
     search = request.GET.get('search', '')
 
     query = um_ecomm_dept_units_rept.objects.filter(dept_grp_vp_area__contains=search).order_by().values_list('dept_grp_vp_area','dept_grp_vp_area_descr').distinct()
-    vps = list(query)
-    print vps
+    vp_groups =list(query)
 
-    tree = []
-    
-    for vp in vps:
-      query = um_ecomm_dept_units_rept.objects.filter(dept_grp=vp[0]).order_by().values_list('dept_grp','dept_grp_descr').distinct()
+    final_groups = []
+
+    # each vp is a tuple of 2 
+    for vp in vp_groups:
+
+      #get all dept_grps from each vp_group
+      query = um_ecomm_dept_units_rept.objects.filter(dept_grp_vp_area=vp[0]).order_by().values_list('dept_grp','dept_grp_descr').distinct()
+
       vp = list(vp)
       vp.append(list(query)) # vp[2] is now the list of dept_grps associated with that thing
 
+      # each dept_grp is a tuple of 2
       idx = 0
-      for group in vp[2]:
-        query = um_ecomm_dept_units_rept.objects.filter(dept_grp=group[0]).order_by().values_list('deptid','dept_descr').distinct()
+      for dept_grp in vp[2]:
+        query = um_ecomm_dept_units_rept.objects.filter(dept_grp=dept_grp[0]).order_by().values_list('deptid','dept_descr').distinct()
 
         vp[2][idx] = list(vp[2][idx])
         vp[2][idx].append(list(query))
         idx += 1
 
-      tree.append(vp)
+      final_groups.append(vp)
 
-    print tree
+    print final_groups
+        
 
-    return render(request, 'tree-dynamic.html', {'d': tree})
+
+    #vps = list(query)
+
+    #tree = []
+    #
+    #for vp in vps:
+    #  query = um_ecomm_dept_units_rept.objects.filter(dept_grp=vp[0]).order_by().values_list('dept_grp','dept_grp_descr').distinct()
+    #  vp = list(vp)
+    #  vp.append(list(query)) # vp[2] is now the list of dept_grps associated with that thing
+
+    #  idx = 0
+    #  for group in vp[2]:
+    #    query = um_ecomm_dept_units_rept.objects.filter(dept_grp=group[0]).order_by().values_list('deptid','dept_descr').distinct()
+
+    #    vp[2][idx] = list(vp[2][idx])
+    #    vp[2][idx].append(list(query))
+    #    idx += 1
+    #    print vp
+
+    #  tree.append(vp)
+
+    #print tree
+
+    return render(request, 'tree-dynamic.html', {'d': final_groups})
 
     #get all unique bud seqs that match search,
     # for each bud seq, get all unique vp grps 
