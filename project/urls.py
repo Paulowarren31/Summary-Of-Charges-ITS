1c2796bf7dc67a1d3d8c73836c572dc742778bd5
paulowar <paulowar@umich.edu>
2017-08-03 10:35:51 -0400

    missing import

--- project/urls.py
+++ project/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
 
 from soc.views import index, table, departments, dept_info, download, search
 
